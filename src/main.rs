4234598239e37794b6eb29e922cf66b8c9113f27
Charlie Govea <67932262+charlie-n01r@users.noreply.github.com>
2026-03-11 03:04:29 +0000

    Add file and directory hashing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use clap::{Parser, Subcommand};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use sha2::{Sha256, Digest};
+use env_logger;
+
+use std::io;
+use std::fs;
 
 #[derive(Parser)]
 #[command(name = "integrity-check", version = None)]
@@ -29,35 +33,64 @@ enum Commands {
     },
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let cli = CLI::parse();
-    calculate_hash(b"lol".to_vec());
 
-    /*
     match cli.command {
-        Commands::Init { path } => init(path),
-        Commands::Update { path } => update(path),
-        Commands::Check { path } => check(path),
+        Commands::Init { path } => for_each_file(&path, &mut init),
+        Commands::Update { path } => for_each_file(&path, &mut update),
+        Commands::Check { path } => for_each_file(&path, &mut check),
     }
-    */
 }
 
-fn init(path: PathBuf) {
+fn init(path: &Path) -> io::Result<()> {
     println!("Initializing hashes for {:?}", path);
-}
-
-fn update(path: PathBuf) {
-    println!("Updating hashes for {:?}", path);
-}
-
-fn check(path: PathBuf) {
-    println!("Checking hashes for {:?}", path);
-}
-
-fn calculate_hash(contents: Vec<u8>) {
-    let hash = Sha256::digest(contents);
+    let hash = calculate_hash(path);
     let hex: String = hash.iter()
         .map(|b| format!("{:02x}", b))
         .collect();
-    println!("{hex:?}");
+    println!("{hex}");
+
+    Ok(())
+}
+
+fn update(path: &Path) -> io::Result<()> {
+    println!("Updating hashes for {:?}", path);
+    Ok(())
+}
+
+fn check(path: &Path) -> io::Result<()> {
+    println!("Checking hashes for {:?}", path);
+    Ok(())
+}
+
+fn calculate_hash(path: &Path) -> [u8; 32] {
+    let contents = fs::read(path).unwrap();
+    Sha256::digest(contents).into()
+}
+
+
+fn for_each_file<F>(path: &Path, fun: &mut F) -> io::Result<()>
+where
+    F: FnMut(&Path) -> io::Result<()>,
+{
+    if path.is_file() {
+        fun(path)?;
+    } else if path.is_dir() {
+        for entry in fs::read_dir(path)? {
+            let entry = entry?;
+            let entry_path = entry.path();
+
+            // If entry is a file, apply the closure
+            if entry_path.is_file() {
+                fun(&entry_path)?;
+            }
+            // If it's a directory, recurse
+            else if entry_path.is_dir() {
+                for_each_file(&entry_path, fun)?;
+            }
+        }
+    }
+
+    Ok(())
 }
